6c144fc9836eec1467812ad8dcdd00d92b89f686
Daniel McCormack <daniel.mccormack@sas.com>
2024-10-19 13:53:41 -0400

    first-commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,64 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'ashley-cpa';
+
+  ngOnInit() {
+    function hello(element: any) {
+      console.log("hey");
+  }
+  
+  document.addEventListener('DOMContentLoaded', function () {
+      const sections = document.querySelectorAll('section');
+  
+      const options = {
+          threshold: 0.1,
+          rootMargin: "0px 0px -50px 0px"
+      };
+  
+      const observer = new IntersectionObserver(function(entries, observer) {
+          entries.forEach(entry => {
+              if (!entry.isIntersecting) {
+                  return;
+              }
+              console.log("showing...");
+              console.log(entry);
+              entry.target.classList.add('show');
+              observer.unobserve(entry.target);
+          });
+      }, options);
+  
+      sections.forEach(section => {
+          observer.observe(section);
+      });
+  
+      const mobileMenuIcon = document.querySelector('.mobile-menu-icon');
+      const menu = document.querySelector('.menu');
+  
+      mobileMenuIcon!.addEventListener('click', () => {
+          menu!.classList.toggle('active');
+          mobileMenuIcon!.classList.toggle('active');
+  
+          
+          // const menuItems = document.querySelectorAll('.menu a');
+          // menuItems.forEach(function(item) {
+          //     item.addEventListener('click', () => {
+          //         menu.classList.toggle('active');
+          //         mobileMenuIcon.classList.toggle('active');
+          //     });
+          // })
+      });
+  });
+  
+  }
+}
+
+
